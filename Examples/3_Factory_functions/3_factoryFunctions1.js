7ccc9a7638aedf6f36948feca9410ccaff90b2a7
Rodrigo <rvidela@outlook.com>
2019-08-22 18:41:22 -0300

    Spelling checked, lot of other improvements too.

--- Examples/3_Factory_functions/3_factoryFunctions1.js
+++ Examples/3_Factory_functions/3_factoryFunctions1.js
@@ -6,11 +6,26 @@ function Rectangle(length, breadth) {
       return this.length * this.breadth;
     }
   };
-};
+}
+
+// Function to print the data
+// Rectangle.prototype.otrafuncion = () => {
+//   console.log(this);
+//   console.log(typeof this);
+//   console.log(this.length);
+//   console.log(this.breadth);
+// };
 
 const square = Rectangle(10, 10);
+const square2 = new Rectangle(10, 10);
 
 square.printAll = function() {
+  console.log(`length: ${this.length}`);
+  console.log(`breadth: ${this.breadth}`);
+  console.log(`area: ${this.getArea()}`);
+};
+
+square2.printAll = function() {
   console.log(`length: ${this.length}`);
   console.log(`breadth: ${this.breadth}`);
   console.log(`area: ${this.getArea()}`);
@@ -19,3 +34,13 @@ square.printAll = function() {
 console.log(square);
 console.log(square.getArea());
 square.printAll();
+// square2.otrafuncion();
+
+console.log(square2);
+console.log(square2.getArea());
+square2.printAll();
+// square2.otrafuncion();
+
+console.log(Rectangle);
+console.log(Rectangle.prototype);
+console.log(Rectangle.prototype.otrafuncion);
--- Examples/5_Class/5_1_constructorFunction2.js
+++ Examples/5_Class/5_1_constructorFunction2.js
@@ -6,15 +6,47 @@ function Rectangle(length, breadth) {
 
   // Function to calculate area
   this.getArea = function() {
+    console.log(this);
+    console.log(typeof this);
+    console.log(this.length);
+    console.log(this.breadth);
     return this.length * this.breadth;
   };
-  // Function to print the data
-  this.printAll = function() {
-    console.log(`The length is: ${this.length}`);
-    console.log(`The breadth is: ${this.breadth}`);
-    console.log(`The area is: ${this.getArea()}`);
-  };
 }
+
+// Function to print the data
+Rectangle.prototype.printAll = function() {
+  console.log(this);
+  console.log(typeof this);
+  console.log(this.length);
+  console.log(this.breadth);
+  console.log(`The length is: ${this.length}`);
+  console.log(`The breadth is: ${this.breadth}`);
+  console.log(`The area is: ${this.getArea()}`);
+};
+
+// Function to print the data
+Rectangle.prototype.otrafuncion = () => {
+  console.log(this);
+  console.log(typeof this);
+  console.log(this.length);
+  console.log(this.breadth);
+};
+
+// Function to print the data
+Rectangle.prototype.otrafuncion2 = () => {
+  console.log(this);
+  console.log(typeof this);
+  console.log(this.length);
+  console.log(this.breadth);
+};
+
 const square = new Rectangle(10, 10); // Create object
-console.log(square.getArea()); // Returns area, i.e. 100
+// console.log(square.getArea()); // Returns area, i.e. 100
+const square2 = Rectangle(10, 10);
 square.printAll();
+square.otrafuncion();
+console.log(square2);
+
+console.log(Rectangle);
+console.log(Rectangle.prototype);
